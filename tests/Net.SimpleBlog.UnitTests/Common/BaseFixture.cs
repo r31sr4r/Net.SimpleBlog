ae77920e841a2e3ceafe49bfdb24d97c8f9a5d20
Rafael Reis <rafael.reis.a@gmail.com>
2024-05-18 12:09:36 -0300

    test: add password hasher unit tests

---
+++ tests/Net.SimpleBlog.UnitTests/Common/BaseFixture.cs
@@ -0,0 +1,10 @@
+﻿using Bogus;
+
+namespace Net.SimpleBlog.UnitTests.Common;
+public abstract class BaseFixture
+{
+    protected BaseFixture()
+        => Faker = new Faker("pt_BR");
+
+    public Faker Faker { get; set; }
+}
---
+++ tests/Net.SimpleBlog.UnitTests/Common/Security/PasswordHasherTest.cs
@@ -0,0 +1,33 @@
+﻿using Net.SimpleBlog.Domain.Common.Security;
+using Xunit;
+
+namespace Net.SimpleBlog.UnitTests.Common.Security;
+public class PasswordHasherTest
+{
+    [Fact(DisplayName = nameof(ShouldGenerateHash_ForGivenPassword))]
+    [Trait("Common/Security", "PasswordHasher")]
+    public void ShouldGenerateHash_ForGivenPassword()
+    {
+        var password = "TestPassword123!";
+
+        var hashedPassword = PasswordHasher.HashPassword(password);
+
+        Assert.NotEmpty(hashedPassword);
+        Assert.NotEqual(password, hashedPassword);
+    }
+
+    [Theory(DisplayName = nameof(ShouldValidatePasswordCorrectly))]
+    [Trait("Common/Security", "PasswordHasher")]
+    [InlineData("MySecurePassword!", true)]
+    [InlineData("WrongPassword", false)]
+    public void ShouldValidatePasswordCorrectly(string testPassword, bool expectedResult)
+    {
+        var originalPassword = "MySecurePassword!";
+        var hashedPassword = PasswordHasher.HashPassword(originalPassword);
+
+        var result = PasswordHasher.VerifyPasswordHash(testPassword, hashedPassword);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
+
